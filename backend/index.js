f16a805ee23140c9c7565678d6ff15bfbbfaeff0
David Pinilla <43119066+DavidPinilla1@users.noreply.github.com>
2019-09-13 09:38:57 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const morgan=require('morgan');
 const winston=require('./config/winston.js');
 const userRouter=require('./routes/users.js');
-const port= proccess.env.PORT || 3001;
+const port= process.env.PORT || 3001;
 // app.use( function ( req, res, next ) {
 //     res.header( "Access-Control-Allow-Origin", "*" );
 //     res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
